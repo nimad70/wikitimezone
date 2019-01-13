7d111d75edcb2d2687f5f05b72b3b605c626b493
nima daryabar <nima.daryabar@yahoo.com>
2019-01-13 17:42:39 +0330

    wikipedia.py - add soup and find tr tags in main big table

--- wikipedia.py
+++ wikipedia.py
@@ -1,3 +1,6 @@
+# Author:nima daryabar
+# Show a timezone table using wikipedia
+
 from bs4 import BeautifulSoup
 import re
 
@@ -6,13 +9,37 @@ from scrape import webscraping
 
 print("https://www.wikipedia.org/")
 print
+""" URL to send request """
 wiki_url = 'https://en.wikipedia.org/wiki/List_of_tz_database_time_zones'
+""" Get response for the specific url """
 res = webscraping(wiki_url)
-wiki_text_page = res.text
 # print(page)
-# print page
 print
-wiki_soup = BeautifulSoup(wiki_text_page, 'html.parser')
-# print(wiki_soup)
+""" Pars as html using bs4 lib """ 
+soup = BeautifulSoup(res.text, 'html.parser')
+print(soup.title.text)
+# print(soup)
+""" Find tables by class attribute 'wikitable' """
+# wikitables = soup.find_all('table', attrs={'class': 'wikitable'})
+# print(len(wikitables))
+# print(wikitables)
 
+""" tbody #1 - main table """
+# print(wikitables[1])
+# res1 = wikitables[1]
+# print(res1.text)
+
+""" tbody #2 """
+# print(wikitables[0])
+# res0 = wikitables[0]
+# print(res0.text)
+
+""" Find all tables with table tag """
+wikitable = soup.findAll('table')
+all_tr = wikitable[0].findAll('tr')
+# print(all_tr)
+# print
+for t in all_tr:
+    print
+    print (t.text)
 
